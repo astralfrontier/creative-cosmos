51599e252dad6acc87a93304bb160f5fc6650436
Bill Garrett <garrett@astralfrontier.org>
2024-04-26 06:39:22 -0700

    Add an Open Graph scraper

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # creative-cosmos
 A catalog of interesting websites for creative expression
+
+## Entries
+
+Each entry is its own file, with the `.md` extension. Entries live in the `entries` directory. Entries can be placed in nested subdirectories.
+
+Entries have YAML front-matter with the following keys:
+
+- name: the formal name for the entry
+- url: the URL where you can find the entry
+- imageUrl: the original source of the image associated with the entry
+- tags: an array of tags (which must appear in tags.yaml)
+
+The body of the entry's file is Markdown which is treated as the description for the entry.
+
+To add an image, add it with the same filename, but with the `.png`, `.jpg`, etc. extension.
--- gulpfile.ts
+++ gulpfile.ts
@@ -1,8 +1,68 @@
+import fs from "node:fs";
+import path from "node:path";
+import ogs from "open-graph-scraper";
+import { prompt } from "enquirer";
+import yaml from "yaml";
+import { pathOr, pluck } from "ramda";
+import slugify from "slugify";
+
+const tagDictionaryPath = path.join(__dirname, "tags.yaml");
+const tagDictionary = yaml.parse(fs.readFileSync(tagDictionaryPath).toString());
+
 type GulpCallback = (error?: any) => void;
+
+async function scrapeUrl() {
+  const options = await prompt({
+    type: "input",
+    name: "url",
+    message: "URL to include?",
+    initial: "https://ogp.me",
+  });
+  const { result } = await ogs(options);
+  const choices = pluck("name", tagDictionary);
+  const frontmatter: Record<string, any> = await prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Title",
+      initial: result.ogTitle,
+    },
+    {
+      type: "input",
+      name: "imageUrl",
+      message: "Image URL",
+      initial: pathOr("", ["ogImage", 0, "url"], result),
+    },
+    {
+      type: "autocomplete",
+      name: "tags",
+      message: "Tags? (space selects)",
+      choices,
+      multiple: true,
+    },
+  ]);
+
+  const filename = path.join(
+    __dirname,
+    "entries",
+    slugify(frontmatter["title"] as string, {
+      lower: true, // convert to lower case, defaults to `false`
+      strict: true, // strip special characters except replacement, defaults to `false`
+      trim: true, // trim leading and trailing replacement chars, defaults to `true`
+    })
+  );
+
+  const contents = `---\n${yaml.stringify(frontmatter)}---\n\n${
+    result.ogDescription || "No description"
+  }\n`;
+
+  fs.writeFileSync(`${filename}.md`, contents);
+  // TODO: cache image
+}
 
 function defaultTask(cb: GulpCallback) {
   console.log("hello world");
   cb();
 }
 
-exports.default = defaultTask;
+exports.default = scrapeUrl;
